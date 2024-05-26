0eeacb619ed1981fc9e8b818e03a5a3d234b4815
Sean Rodriguez <sean.rodriguez@outlook.com>
2024-05-26 17:12:56 -0400

    Initial commit

---
+++ apps/michaels-startdate/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'michaels-startdate',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/michaels-startdate',
+};
